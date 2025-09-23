3a83f7c1c7f01f86042189d74f86422593824c67
khanh <tintucsukien09876@gmail.com>
2025-09-24 00:29:19 +0700

    first commit

---
+++ context/Theme.tsx
@@ -0,0 +1,9 @@
+import type { ThemeProviderProps } from "next-themes";
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+import React from "react";
+
+const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+};
+
+export default ThemeProvider;
