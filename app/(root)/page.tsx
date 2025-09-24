5cc02e177ab183743f5affd3280c81574d748d1c
khanh <tintucsukien09876@gmail.com>
2025-09-24 23:33:23 +0700

    third

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
 export default function Home() {
   return (
     <>
       <h1>Welcome to my website</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_OUT });
+        }}
+      >
+        <Button className="cursor-pointer  bg-white text-black" type="submit">
+          Signout with GitHub
+        </Button>
+      </form>
     </>
   );
 }
