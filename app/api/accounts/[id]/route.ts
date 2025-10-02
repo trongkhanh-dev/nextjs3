06819f18abb65f49bc9154c81e60dd8d915a2129
khanh <tintucsukien09876@gmail.com>
2025-10-02 09:25:22 +0700

    dynamicaccountroute

---
+++ app/api/accounts/[id]/route.ts
@@ -0,0 +1,70 @@
+import Account from "@/database/account.model";
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema, UserSchema } from "@/lib/validation";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  try {
+    await dbConnect();
+    const account = await Account.findById(id);
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  try {
+    await dbConnect();
+    const account = await User.findByIdAndDelete(id);
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  try {
+    await dbConnect();
+    const body = await request.json();
+    const validatedData = AccountSchema.partial().safeParse(body);
+
+    if (!validatedData.success)
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+
+    const updatedAccount = await Account.findByIdAndUpdate(id, validatedData, {
+      new: true,
+    });
+
+    if (!updatedAccount) throw new NotFoundError("Account");
+
+    return NextResponse.json(
+      { success: true, data: updatedAccount },
+      { status: 200 }
+    );
+  } catch (error) {
+    handleError(error, "api") as APIErrorResponse;
+  }
+}
